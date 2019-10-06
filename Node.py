2f499aea1bfe5a31e06a22a31381b4c995b7305f
vcandeias <vascocandeias@tecnico.ulisboa.pt>
2019-10-06 13:15:12 +0100

    Cleaning and commenting

--- Node.py
+++ Node.py
@@ -20,24 +20,26 @@ class Node:
 		return self.nexthop
 
 	def deletePath(self, prefix):
+		"""Recursive method to delete a prefix and the path from the root to that node"""
 
+		# if this is the node to delete, set its nexthop to None to clear the entry
 		if prefix is "":
 			self.nexthop = None
 		else:
+			# otherwise continue down the path
 			bit = int(prefix[0])
 			rest = prefix[1:]
 
+			# if the prefix doesn't exist, abort
 			if self.child[bit] is None:
 				return False
+			# otherwise go down the path
 			elif self.child[bit].deletePath(rest):
-				print("here")
+				# if the deletition returns True, the child should be deleted
 				self.child[bit] = None
 
-		if self.nexthop is None and all(x is None for x in self.child):
-			return True
-		else:
-			return False
-
+		# return whether the node is now a leaf. if it is, it should be deleted
+		return self.nexthop is None and all(x is None for x in self.child)
 
 	def recursiveCompression(self, nexthop):
 		"""Compress the tree using only aggregation and filtering"""
@@ -66,9 +68,13 @@ class Node:
 		return False
 
 	def ORTCStep1(self, nexthop):
+		"""Applies the first step of the ORTC algorithm"""
+
+		# if a node has no nexthop, it inherits his parent's
 		if self.nexthop is None:
 			self.nexthop = nexthop
 		else:
+			# otherwise turn the value into a list
 			self.nexthop = [self.nexthop]
 
 		# if the node only has one child, create the other
@@ -91,23 +97,32 @@ class Node:
 			self.nexthop = self.child[0].nexthop + self.child[1].nexthop
 
 	def intersection(self): 
+		"""Returns a list containing the intersection of two lists"""
 		return list(set(self.child[0].nexthop) & set(self.child[1].nexthop)) 
 
 	def ORTCStep2(self, nexthop):
+		"""Applies the final step of the ORTC algorithm"""
+
+		# clear redundant nexthops
 		if nexthop in self.nexthop:
 			self.nexthop = None
 		else:
+			# otherwise choose the first of the list
 			self.nexthop = self.nexthop[0]
 			nexthop = self.nexthop
 
+		# apply thw algoritm to both children
 		for i in range(2):
 			if self.child[i] is not None:
 				if self.child[i].ORTCStep2(nexthop):
+					# remove the child if it is an empty leaf
 					self.child[i] = None
 
+		# if the node is an empty leaf, signal its deletition
 		if self.nexthop is None and all(x is None for x in self.child):
 			return True
 
+		# otherwise, don't delete
 		return False
 
 	def display(self):
